295826c33d4b85fffc706ebab276bb8eeb4024b2
Ejercicio1 <diandiesurl@gmail.com>
2025-05-18 14:59:37 -0600

    Lab03 - Diego Diaz - 1214024

--- src/main/java/ed/lab/E01TopKFrequentElements.java
+++ src/main/java/ed/lab/E01TopKFrequentElements.java
@@ -1,7 +1,28 @@
 package ed.lab;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class E01TopKFrequentElements {
     public int[] topKFrequent(int[] nums, int k) {
-        return null;
+            Map<Integer, Integer> frequent = new HashMap<>();
+
+            for(int num : nums) {
+                frequent.putIfAbsent(num, 1);
+                frequent.put(num, frequent.get(num) + 1);
+            }
+
+            Queue<Integer> heap = new PriorityQueue<Integer>((a, b) -> frequent.get(b) - frequent.get(a));
+            heap.addAll(frequent.keySet());
+
+            int[] result = new int[k];
+
+            for(int i = k-1; i >= 0; i--) {
+                result[i] = heap.poll();
+            }
+
+            return result;
     }
 }
--- src/main/java/ed/lab/E02AutocompleteSystem.java
+++ src/main/java/ed/lab/E02AutocompleteSystem.java
@@ -1,13 +1,82 @@
 package ed.lab;
-import java.util.List;
+import java.util.*;
 
 public class E02AutocompleteSystem {
+    static class TrieNode {
+        Map<Character, TrieNode> children = new HashMap<>();
+        boolean isLast = false;
+        }
+
+        TrieNode root;
+        StringBuilder SB = new StringBuilder();
+        Map<String, Integer> frequency = new HashMap<>();
+        TrieNode current;
 
     public E02AutocompleteSystem(String[] sentences, int[] times) {
+        root = new TrieNode();
 
+
+        for(int i = 0; i < sentences.length; i++) {
+            frequency.put(sentences[i], times[i]);
+            add(sentences[i]);
+        }
+
+        current = root;
+    }
+
+    public void add(String words) {
+        TrieNode current = root;
+
+        List<TrieNode> wordvisited = new ArrayList<>();
+        for(char c : words.toCharArray()) {
+            current = current.children.computeIfAbsent(c, k -> new TrieNode());
+        }
+        current.isLast = true;
     }
 
     public List<String> input(char c) {
-        return null;
+
+        if(c == '#') {
+            String Sentence = SB.toString();
+            frequency.put(Sentence, frequency.getOrDefault(Sentence, 0) + 1);
+            add(Sentence);
+            SB.setLength(0);
+            current = root;
+            return new ArrayList<>();
+        }
+
+        SB.append(c);
+        if(current != null) {
+            current = current.children.get(c);
+        }
+
+        if(current == null) {
+            return new ArrayList<>();
+        }
+
+        List<String> matches = new ArrayList<>();
+        dfs(current, SB.toString(), matches);
+
+        PriorityQueue<String> heap = new PriorityQueue<>((a,b) -> frequency.get(a).equals(frequency.get(b)) ? a.compareTo(b) : frequency.get(b) - frequency.get(a));
+
+        for(String match : matches) {
+            heap.offer(match);
+        }
+
+        List<String> result = new ArrayList<>();
+        for(int i = 0; i < 3 && !heap.isEmpty(); i++) {
+            result.add(heap.poll());
+        }
+        return result;
+    }
+
+    private void dfs(TrieNode node, String sentence, List<String> result) {
+        if(node.isLast && frequency.containsKey(sentence)) {
+            result.add(sentence);
+        }
+
+        for(Map.Entry<Character, TrieNode> entry : node.children.entrySet()) {
+            dfs(entry.getValue(), sentence + entry.getKey(), result);
+        }
     }
 }
